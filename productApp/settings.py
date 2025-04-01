4b5ee5e2550185bb0cd18885470eb39fca2a9be7
meaSec <muhammetemiraslan0@gmail.com>
2025-04-02 01:29:14 +0300

    anasayfanın tamamlanması ve footer'ın partial edilip dinamik hale getirilmesi

--- productApp/settings.py
+++ productApp/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "siteapp.context_processors.site_info"
             ],
         },
     },
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "tr-tr"
 
 TIME_ZONE = "UTC"
 
---
+++ siteapp/context_processors.py
@@ -0,0 +1,2 @@
+def site_info(request):
+    return {"copyright_year": 2025, "company_name": "AIKE"}
