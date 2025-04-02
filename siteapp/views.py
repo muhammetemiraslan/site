7c109bb553e1c9735044dd9c436c604dc333a6c7
meaSec <muhammetemiraslan0@gmail.com>
2025-04-02 23:59:56 +0300

    index product dinamik veri güncellemesi

--- siteapp/views.py
+++ siteapp/views.py
@@ -4,7 +4,15 @@ from .models import product, category
 # Create your views here.
 
 def home(request):
-    return render(request, "siteapp/index.html")
+    products = product.objects.all()[:3]
+    
+    categories = category.objects.all()
+
+    return render(request, "siteapp/index.html",
+    {
+        "products":products,
+        "categories":categories,
+    })
 
 def Category(request):
     return render(request, "siteapp/category.html")
